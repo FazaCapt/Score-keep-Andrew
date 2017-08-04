546520ee98d1538ecdcedbba8890007809fa4eba
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-04 17:23:33 +0700

    ngulang lagi  Importing and Exporting

---
+++ imports/utils2.js
@@ -0,0 +1,7 @@
+console.log('Log from /imports/utils2.js');
+
+export let greetUser = function () {
+    return 'Hello User!';
+};
+
+export let name = 'Faza Fahamsyah';
--- server/main.js
+++ server/main.js
@@ -1,6 +1,12 @@
 // import './../imports/utils';
-import {greetUser} from './../imports/utils';
-import add from './../imports/math';
+// import {greetUser} from './../imports/utils';
+// import add from './../imports/math';
+// import './../imports/utils2';
+import {greetUser, name} from './../imports/utils2';
+import add from './../imports/math2';
 
 console.log('Log from /server/main.js');
+console.log(greetUser());
 console.log(add(8, 7));
+
+// console.log(add(8, 7));
