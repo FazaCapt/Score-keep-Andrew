2dfd2af771c7d33742fa95dfc147f1707ddccc3a
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-08-04 18:12:08 +0700

    Render Complex HTML with JSX

--- client/main.js
+++ client/main.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import {meteor} from 'meteor/meteor';
 
 Meteor.startup(function () {
+    // title -> Account settings
+    let title = 'Account Settings';
     let name = 'Faza'
-    let jsx = <p>Hello {name}!</p>;
+    let jsx = (
+    <div>
+        {/* Render h1 tag with title var as text */}
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+    </div>
+    );
     ReactDOM.render(jsx, document.getElementById('app'));
 });
